c733f0de2b7688101f8d3e38a9ff96f86e1e9735
Giridhar S <sgiridhar14@gmail.com>
2017-03-22 19:42:57 +0530

    Number Check

---
+++ numchk.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+cout<<"Enter number";
+cin>>num;
+if(num>0)
+{
+cout<<"Number "<<num<<"is a Positive number";
+}
+else if(num==0)
+{
+cout<<"Number is zero";
+}
+else
+{
+cout<<"Number: "<<num<<"is a Negative number";
+}
+}
